027b124ce2fcd638f5687fabeafd3e051085f24c
alek <sasho2000@gmail.com>
2024-08-29 23:38:55 -0400

    back up and running

--- client/run.py
+++ client/run.py
@@ -5,7 +5,7 @@ from internal_settings.internal import get_settings
 
 
 def run():
-    client = MyClient()
+    client = MyClient(intents=discord.Intents.all())
     print("\nStarting Process...\n")
     token = get_settings()
     client.today_date = datetime.now().strftime("%d")
